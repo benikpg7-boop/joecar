88f75d42612afc2135e97e9bebd2ccbd07080f77
benikpg7-boop <benikpg7@gmail.com>
2026-06-13 11:18:16 +0000

    Add new car to initial localStorage data

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const voituresInitiales = [
     { id: 1, marque: "Toyota", modele: "Prado", prix: 65000, categorie: "SUV", carburant: "diesel", transmission: "automatique", places: 7, image: "prado.jpg" },
     { id: 2, marque: "Mercedes", modele: "Classe S", prix: 150000, categorie: "luxe", carburant: "sans plomb", transmission: "automatique", places: 5, image: "merco.webp" },
     { id: 3, marque: "Kia", modele: "Picanto", prix: 25000, categorie: "economique", carburant: "sans plomb", transmission: "manuelle", places: 5, image: "kia.webp" }
+    { id: 4, marque: "Geely", modele: "GX3 Pro", prix: 55000, categorie: "Luxe", carburant: "sans plomb", transmission: "automatique", places: 5, image: "geelygx3pro.webp" }  
 ];
 
 // Récupérer les voitures du localStorage ou charger les initiales
